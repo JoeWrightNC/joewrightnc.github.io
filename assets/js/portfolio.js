c6710b148823658d5a4a6ac7eea540b6394bb662
Joe Wright <joewrightart@gmail.com>
2018-03-21 16:37:43 -0400

    landing fully built up to loading nav

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,11 +1,50 @@
-$("#hamburgerBtn").mouseenter(function(){
-  $("#hamburgerBtn").css("right", "11%");
-})
+
+$("#landingDiv").show()
+$("#navScreenDiv").hide()
+$("#resumeDiv").hide()
+$("#websitesDiv").hide()
+$("#gamesDiv").hide()
+$("designDiv").hide()
+$("artDiv").hide()
+$("#aboutDiv").hide()
+
+function surpriseDivTime() {
+  var i=0
+  $("#surpriseDiv").show()
+  do {
+    $("#surpriseDiv").fadeOut(250);
+    $("#surpriseDiv").fadeIn(250);
+    i++
+  }
+  while (i<13)
+  $("#surpriseDiv").fadeOut(250);
+}
+
+surpriseDivTime()
+
+$("#hamburgerBtn").on("mouseenter", (function(){
+  $("#hamburgerBtn").html("Click to Explore")
+  $("#hamburgerBtn").animate({
+    right: "10%",
+    top: "10%",
+    height: "40px",
+  }, 1000)
+}))
 
 $("#hamburgerBtn").on("click", function(){
   console.log("click received")
+  $("#hamburgerBtn").unbind("mouseenter")
   $("#hamburgerBtn").animate({
     width: "100%",
-    left: "0",
+    right: "48%",
   }, 1800)
+  navScreen()
 })
+
+function navScreen() {
+  $("#landingDiv").hide()
+  $("#navScreenDiv").show()
+  $("#resumeBtn").on("click", function(){
+    
+  })
+}
