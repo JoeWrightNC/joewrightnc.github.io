c31f1f3461b534a7f0c7472e243244a5914921d0
Joe Wright <joewrightart@gmail.com>
2018-03-21 20:16:08 -0400

    built up games page a good bit

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -4,8 +4,8 @@ $("#navScreenDiv").hide()
 $("#resumeDiv").hide()
 $("#websitesDiv").hide()
 $("#gamesDiv").hide()
-$("designDiv").hide()
-$("artDiv").hide()
+$("#designDiv").hide()
+$("#artDiv").hide()
 $("#aboutDiv").hide()
 
 function surpriseDivTime() {
@@ -26,19 +26,26 @@ $("#hamburgerBtn").on("mouseenter", (function(){
   $("#hamburgerBtn").html("Click to Explore")
   $("#hamburgerBtn").animate({
     right: "10%",
-    top: "10%",
+    top: "9.5%",
     height: "40px",
   }, 1000)
 }))
 
 $("#hamburgerBtn").on("click", function(){
   console.log("click received")
+
   $("#hamburgerBtn").unbind("mouseenter")
   $("#hamburgerBtn").html("Destinations")
   $("#hamburgerBtn").animate({
     width: "100%",
     right: "48%",
   }, 1800)
+  $("#resumeDiv").hide()
+$("#websitesDiv").hide()
+$("#gamesDiv").hide()
+$("#designDiv").hide()
+$("#artDiv").hide()
+$("#aboutDiv").hide()
   navScreen()
 })
 
@@ -46,6 +53,111 @@ function navScreen() {
   $("#landingDiv").hide()
   $("#navScreenDiv").show()
   $("#resumeBtn").on("click", function(){
-    
+    $("#hamburgerBtn").html("Explore More?")
+    $("#landingDiv").hide()
+    $("#surpriseDiv").hide()
+    $("#navScreenDiv").hide()
+    $("#resumeDiv").show()
+    $("#websitesDiv").hide()
+    $("#gamesDiv").hide()
+    $("#designDiv").hide()
+    $("#artDiv").hide()
+    $("#aboutDiv").hide()
+    $("#hamburgerBtn").animate({
+      width: "8%",
+      right: "9.5%",
+      position: "absolute",
+      fontSize:"small",
+    }, 1800)
+  })
+  $("#websitesBtn").on("click", function(){
+    $("#hamburgerBtn").html("Explore More?")
+    $("#landingDiv").hide()
+    $("#surpriseDiv").hide()
+    $("#navScreenDiv").hide()
+    $("#resumeDiv").hide()
+    $("#websitesDiv").show()
+    $("#gamesDiv").hide()
+    $("#designDiv").hide()
+    $("#artDiv").hide()
+    $("#aboutDiv").hide()
+    $("#hamburgerBtn").animate({
+      width: "8%",
+      right: "9.5%",
+      position: "absolute",
+      fontSize:"small",
+    }, 1800)
+  })
+  $("#gamesBtn").on("click", function(){
+    $("#hamburgerBtn").html("Explore More?")
+    $("#landingDiv").hide()
+    $("#surpriseDiv").hide()
+    $("#navScreenDiv").hide()
+    $("#resumeDiv").hide()
+    $("#websitesDiv").hide()
+    $("#gamesDiv").show()
+    $("#designDiv").hide()
+    $("#artDiv").hide()
+    $("#aboutDiv").hide()
+    $("#hamburgerBtn").animate({
+      width: "8%",
+      right: "9.5%",
+      position: "absolute",
+      fontSize:"small",
+    }, 1800)
+    })
+  $("#designBtn").on("click", function(){
+    $("#hamburgerBtn").html("Explore More?")
+    $("#landingDiv").hide()
+    $("#surpriseDiv").hide()
+    $("#navScreenDiv").hide()
+    $("#resumeDiv").hide()
+    $("#websitesDiv").hide()
+    $("#gamesDiv").hide()
+    $("#designDiv").show()
+    $("#artDiv").hide()
+    $("#aboutDiv").hide()
+    $("#hamburgerBtn").animate({
+      width: "8%",
+      right: "9.5%",
+      position: "absolute",
+      fontSize:"small",
+    }, 1800)
+  })
+  $("#artBtn").on("click", function(){
+    $("#hamburgerBtn").html("Explore More?")
+    $("#landingDiv").hide()
+    $("#surpriseDiv").hide()
+    $("#navScreenDiv").hide()
+    $("#resumeDiv").hide()
+    $("#websitesDiv").hide()
+    $("#gamesDiv").hide()
+    $("#designDiv").hide()
+    $("#artDiv").show()
+    $("#aboutDiv").hide()
+    $("#hamburgerBtn").animate({
+      width: "8%",
+      right: "9.5%",
+      position: "absolute",
+      fontSize:"small",
+    }, 1800)
+  })
+  $("#aboutBtn").on("click", function(){
+    $("#hamburgerBtn").html("Explore More?")
+    $("#landingDiv").hide()
+    $("#surpriseDiv").hide()
+    $("#navScreenDiv").hide()
+    $("#resumeDiv").hide()
+    $("#websitesDiv").hide()
+    $("#gamesDiv").hide()
+    $("#designDiv").hide()
+    $("#artDiv").hide()
+    $("#aboutDiv").show()
+    $("#hamburgerBtn").animate({
+      width: "8%",
+      right: "9.5%",
+      position: "absolute",
+      fontSize:"small",
+    }, 1800)
   })
 }
