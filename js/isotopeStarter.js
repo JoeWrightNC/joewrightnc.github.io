fd125ac00c9a700c6e4cb90e06b76605bd131923
Joe Wright <joewrightart@gmail.com>
2019-04-14 13:38:27 -0400

    imagesLoaded method

--- js/isotopeStarter.js
+++ js/isotopeStarter.js
@@ -2,21 +2,21 @@
 	"use strict";
 
 	$(document).ready(function() {
-		setTimeout(function() {
-			var $container = $('#portfolio-container');
-			$container.isotope({
-				masonry: {
-					columnWidth: '.portfolio-item'
-				},
-				itemSelector: '.portfolio-item'
-			});
-			$('#filters').on('click', 'li', function() {
-				$('#filters li').removeClass('active');
-				$(this).addClass('active');
-				var filterValue = $(this).attr('data-filter');
-				$container.isotope({ filter: filterValue });
-			});
-		},500);
 	    //Portfolio masonry
+	    var $container = $('#portfolio-container');
+	    $container.imagesLoaded( function() {
+			$container.isotope({
+	        masonry: {
+	            columnWidth: '.portfolio-item'
+	        },
+	        itemSelector: '.portfolio-item'
+			});
+		});
+	    $('#filters').on('click', 'li', function() {
+	        $('#filters li').removeClass('active');
+	        $(this).addClass('active');
+	        var filterValue = $(this).attr('data-filter');
+	        $container.isotope({ filter: filterValue });
+	    });
   });
 })(jQuery);
